b507972be60c05e8e4be4a307eee5ba63ccf7f3c
cesine <cesine@host>
2012-01-10 17:54:38 -0500

    tests pass (as they should)

--- test/TokenizerTest.js
+++ test/TokenizerTest.js
@@ -1,6 +1,6 @@
 TestCase("TokenizerTest", {
     "test find segmentation segment for English": function() {
         var tokenizer = new ilanguage.Tokenizer();
-        assertEquals("", tokenizer.findSegmenter("This isn't much of a corpus to check in."));
+        assertEquals(" ", tokenizer.findSegmenter("This isn't much of a corpus to check in."));
     }
 });
