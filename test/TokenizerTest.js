a6dbf26791c8b4c8fdd68f6491f802b0d6632784
cesine <cesine@host>
2012-01-10 17:49:43 -0500

    tests pass, infrastructure ready

--- test/TokenizerTest.js
+++ test/TokenizerTest.js
@@ -2,9 +2,5 @@ TestCase("GreeterTest", {
     "test greet": function() {
         var greeter = new myapp.Greeter();
         assertEquals("Hello World!", greeter.greet("World"));
-    },
-    "test greet null": function() {
-        var greeter = new myapp.Greeter();
-        assertNull(greeter.greet(null));
     }
 });
