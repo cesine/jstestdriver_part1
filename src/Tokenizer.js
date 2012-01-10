3195b46969d32600f6d502bc1ef64ffafcacda8c
cesine <cesine@host>
2012-01-10 18:23:43 -0500

    added rudamentary tokenizer which splits on most frequent char

--- src/Tokenizer.js
+++ src/Tokenizer.js
@@ -3,6 +3,25 @@ ilanguage = {};
 ilanguage.Tokenizer = function() { };
 
 ilanguage.Tokenizer.prototype.findSegmenter = function(corpus) {
-  
-  return  " ";
+  var frequencies = {};
+
+  for (i in corpus){
+    if(frequencies[corpus[i]] != null ){
+      frequencies[corpus[i]] = parseInt(frequencies[corpus[i]]) + 1;
+      
+    }else{
+      frequencies[corpus[i]] = 1;
+    }
+  }
+
+  var mostFrequentCharCount = "1";
+  var mostFrequentChar = " ";
+  for (i in frequencies ){
+    if(frequencies[i] > mostFrequentCharCount){
+      mostFrequentCharCount= frequencies[i];
+      mostFrequentChar = i;
+    }
+  }
+
+  return  mostFrequentChar;
 };
